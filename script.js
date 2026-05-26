226eb829637673dc95514b596f8b7cac298b94b4
PrimisOnStage <pratham.limitless@gmail.com>
2026-05-26 20:36:42 +0530

    add feature - hover grid box to change background color to a random rgb color

--- script.js
+++ script.js
@@ -2,19 +2,28 @@ const container = document.querySelector(".container");
 const button = document.querySelector("button");
 button.addEventListener("click", defineGrid);
 
-function defineGrid(){
+function random255(){
+    return Math.ceil(Math.random()*255);
+}
 
+function defineGrid(){
+    container.innerHTML = '';   
     let grid_size = parseInt(prompt("Enter a grid size, eg. 64 for (64x64) grid."));
     let div_side = 600/grid_size;
+
     for (let i = 0; i < grid_size**2; i++) {
         let new_div = document.createElement("div");
         new_div.style.boxSizing = "border-box";
         new_div.style.border = "2px solid black";
         new_div.style.width = `${div_side}px`;
         new_div.style.height = `${div_side}px`;
+        new_div.className = "divs"
         container.appendChild(new_div);
-    }
-    
-}
+        new_div.addEventListener("mouseover", function (e){
+            
+            e.target.style.backgroundColor = `rgb(${random255()},${random255()},${random255()})`;
+        });
 
+    }
+}
 
